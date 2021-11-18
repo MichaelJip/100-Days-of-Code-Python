5f0f2c6571ead3c4162ac443099fed1a4bcff2ce
Michael <92431155+MichaelJip@users.noreply.github.com>
2021-11-18 11:18:03 +0700

    Add files via upload

---
+++ 100 Days of Code Python/Day 1 Python/main.py
@@ -0,0 +1,42 @@
+#print("Hello world")
+
+#print("i want to be a data scientist")
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# print("Day 1 - Pyton Print Function")
+# print("The function is declared like this:")
+# print("Print ('what to print')")
+
+# print("Hello World\nHello World")
+
+
+# print("Hello" + " acong")
+# print("hello" + " " + "acong")
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# input("What is your name?\n")
+
+# print("Hello " + input("What is yout name?\n"))
+
+# print(len(input("What is your name? ")))
+
+# name = input("Whats is your name?")
+# print(name)
+
+# a = input("a: ")
+# b = input("b: ")
+
+# print("a: " +b)
+# print("b: " +a)
+
+print("Welcome to the Band Name Generator.")
+name = input("What's name of the city you grew up in?\n")
+last = input("What's your pet name?\n")
+print("your band name could be" + " " + name + " " + last )
